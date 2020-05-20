cd96e32b678592f36daae77435149838800bb65d
dnfatec <danilo@fatecitapetininga.edu.br>
2020-05-20 19:28:46 -0300

    Exemplo sensor noite v2

--- app/src/main/java/com/example/exsensoresn/MainActivity.java
+++ app/src/main/java/com/example/exsensoresn/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "oi git",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Oi versão 02",Toast.LENGTH_LONG).show();
     }
 }
